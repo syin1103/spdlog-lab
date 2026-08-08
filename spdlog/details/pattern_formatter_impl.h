9e35f0df52da697796ea16f4474d06ff3334f059
syin1103 <syin1103@gmail.com>
2026-08-08 10:44:54 +0800

    feat: add empty Formatter
    Signed-off-by: syin1103 <syin1103@gmail.com>

---
+++ spdlog/details/pattern_formatter_impl.h
@@ -0,0 +1,18 @@
+//
+// Copyright(c) 2026 syin1103.
+// Distributed under the MIT License (http://opensource.org/licenses/MIT)
+//
+
+#ifndef SPDLOG_DETAILS_PATTERN_FORMATTER_IMPL_H_
+#define SPDLOG_DETAILS_PATTERN_FORMATTER_IMPL_H_
+
+#include <spdlog/formatter.h>
+
+inline spdlog::PatternFormatter::PatternFormatter(const std::string& pattern) {}
+
+inline void spdlog::PatternFormatter::Format() {}
+
+inline void spdlog::PatternFormatter::CompilePattern(
+    const std::string& pattern) {}
+
+#endif  // SPDLOG_DETAILS_PATTERN_FORMATTER_IMPL_H_
---
+++ spdlog/formatter.h
@@ -0,0 +1,36 @@
+//
+// Copyright(c) 2026 syin1103.
+// Distributed under the MIT License (http://opensource.org/licenses/MIT)
+//
+
+#ifndef SPDLOG_FORMATTER_H_
+#define SPDLOG_FORMATTER_H_
+
+#include <string>
+
+namespace spdlog {
+
+class Formatter {
+ public:
+  virtual ~Formatter() {}
+  virtual void Format() = 0;
+};
+
+class PatternFormatter : public Formatter {
+ public:
+  explicit PatternFormatter(const std::string& pattern);
+
+  PatternFormatter(const PatternFormatter&) = delete;
+  PatternFormatter& opeartor = (const PatternFormatter&) = delete;
+
+  void Format() override;
+
+ private:
+  void CompilePattern(const std::string& pattern);
+};
+
+}  // namespace spdlog
+
+#include <spdlog/details/pattern_formatter_impl.h>
+
+#endif  // SPDLOG_FORMATTER_H_
