63598d6bf5d46ec2ee551acba5c83ad1d2a6a627
syin1103 <syin1103@gmail.com>
2026-08-13 10:11:19 +0800

    feat: add thread_id function
    Signed-off-by: syin1103 <syin1103@gmail.com>

--- spdlog/details/os.h
+++ spdlog/details/os.h
@@ -61,9 +61,19 @@ inline spdlog::log_clock::time_point now() {
 }
 
 inline size_t thread_id() {
-  // TODO(syin1103)
-  return static_cast<size_t>(
-      std::hash<std::thread::id>()(std::this_thread::get_id()));
+  inline size_t thread_id() {
+#ifdef _WIN32
+    return static_cast<size_t>(::GetCurrentThreadId());
+#elif __linux__
+#if defined(__ANDROID__) && defined(__ANDROID_API__) && (__ANDROID_API__ < 21)
+#define SYS_gettid __NR_gettid
+#endif
+    return static_cast<size_t>(syscall(SYS_gettid));
+#else  // Default to standard C++11 (OSX and other Unix)
+    return static_cast<size_t>(
+        std::hash<std::thread::id>()(std::this_thread::get_id()));
+#endif
+  }
 }
 
 }  // namespace os
